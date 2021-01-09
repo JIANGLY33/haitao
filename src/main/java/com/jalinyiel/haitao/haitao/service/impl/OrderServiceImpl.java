f083888f388d13791809e6bc0c059d8b2b777989
于眠 <lingye.jly@alibaba-inc.com>
2021-01-09 17:28:07 +0800

    refactor:buy逻辑改善

--- src/main/java/com/jalinyiel/haitao/haitao/service/impl/OrderServiceImpl.java
+++ src/main/java/com/jalinyiel/haitao/haitao/service/impl/OrderServiceImpl.java
@@ -77,6 +77,9 @@ public class OrderServiceImpl implements OrderService {
             orderItemVos.add(buyItem);
             return orderItemVos;
         }
+        if (BizOrderConstant.ONLY_SUB == bizOrder.getType()) {
+            return new ArrayList<>();
+        }
         List<BizOrder> subOrders = bizOrderMapper.findByParentId(orderId);
         orderItemVos = subOrders.stream().map(this::generateOrderItemVo).collect(Collectors.toList());
         return orderItemVos;
